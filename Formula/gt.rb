7e5234d1ac29302f0037a89186feba9b27e56e0f
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2025-11-07 16:36:36 +0000

    formula

---
+++ Formula/gt.rb
@@ -0,0 +1,20 @@
+class Gt < Formula
+  desc "GitTool — centraliza e automatiza comandos Git usados pela Elara"
+  homepage "https://github.com/ElaraDevSolutions/gittool"
+  url "https://github.com/ElaraDevSolutions/gittool/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "MIT"
+
+  def install
+    libexec.install Dir["src/*"]
+
+    (bin/"gt").write <<~EOS
+      #!/usr/bin/env bash
+      exec "#{libexec}/gt.sh" "$@"
+    EOS
+  end
+
+  test do
+    system "#{bin}/gt", "--help"
+  end
+end
