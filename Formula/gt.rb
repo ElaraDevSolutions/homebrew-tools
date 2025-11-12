31a009cf8d2585354b2e901c46b8cfbddbe0ed6e
Marcos Rosa <marcos.cantor@gmail.com>
2025-11-12 10:54:05 +0000

    gt: update to v1.0.12

--- Formula/gt.rb
+++ Formula/gt.rb
@@ -1,8 +1,8 @@
 class Gt < Formula
-  desc "GitTool — centraliza e automatiza comandos Git usados pela Elara"
+  desc "GitTool — centralizes and automates Git commands used by Elara Dev Solutions"
   homepage "https://github.com/ElaraDevSolutions/gittool"
-  url "https://github.com/ElaraDevSolutions/gittool/archive/refs/tags/v1.0.11.tar.gz"
-  sha256 "a95ccbb341ffde9aa8c4ae8f90dfbeb7ac6582caebf5ae4858adf4054899d582"
+  url "https://github.com/ElaraDevSolutions/gittool/archive/refs/tags/v1.0.12.tar.gz"
+  sha256 "7471654a4ccc9807312cb6ec7ad5652e628a0c26bf30c6a67416cceaebacd614"
   license "MIT"
   depends_on "fzf"
   depends_on "git"
